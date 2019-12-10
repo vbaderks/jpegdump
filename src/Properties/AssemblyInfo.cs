2c4506f2db6b24ec2bf7982ed49b61e3cdd28c33
Victor Derks <vbaderks@gmail.com>
2019-12-10 12:28:26 +0100

    Remove AssemblyInfo
    The .NET Core docs are not 100% clear, but setting ComVisible(false) is not really needed anymore.

--- src/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Victor Derks.
-// SPDX-License-Identifier: MIT
-
-using System.Runtime.InteropServices;
-
-// Setting ComVisible to false (default is true) makes the types in this assembly not visible to COM components.
-[assembly: ComVisible(false)]
